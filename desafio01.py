c45c16632a59d77ca1cfcaaf357485806115022f
Tamylinmaeske <tamylinmaeske@gmail.com>
2024-05-03 14:06:34 -0300

    Desafios básicos

---
+++ desafio01.py
@@ -0,0 +1,3 @@
+nome = input ("Olá, qual é o seu nome? ")
+linguagemfavorita = input ("Tudo bem com você? Qual a sua linguagem favorita? ")
+print ("Legal! A minha atualmente é a linguagem Python.")
---
+++ desafio03.py
@@ -0,0 +1,4 @@
+numero1= int (input("Digite o primeiro número a ser somado: "))
+numero2= int (input("Digite o segundo número a ser somado: "))
+soma= numero1+numero2
+print ("O resultado da soma é,", soma)
