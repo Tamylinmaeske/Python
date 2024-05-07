6d3d3fac612b9cf6a2c08b723387c0a4c2dc734a
Tamylinmaeske <tamylinmaeske@gmail.com>
2024-05-07 14:49:55 -0300

    Desafios atualizados

--- desafio03.py
+++ desafio03.py
@@ -1,4 +1,4 @@
 numero1= int (input("Digite o primeiro número a ser somado: "))
 numero2= int (input("Digite o segundo número a ser somado: "))
 soma= numero1+numero2
-print ("O resultado da soma é, {}.".format(soma))
+print ("A soma entre {} e {} é igual a {}.".format(numero1, numero2, soma))
