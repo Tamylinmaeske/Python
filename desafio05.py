781839dbd34a3ab25681557701dd0ff0749ef213
Tamylinmaeske <tamylinmaeske@gmail.com>
2024-05-08 20:51:38 -0300

    Atualização de desafios

--- desafio05.py
+++ desafio05.py
@@ -1,1 +1,6 @@
 # Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e antecessor.
+
+n = int (input('Digite um número inteiro: '))
+s = n+1
+a = n-1
+print ('O valor escolhido foi {}.\nO antecessor desse número é o {}.\nO sucessor é o {}.'.format(n, a, s))
--- desafio06.py
+++ desafio06.py
@@ -1,1 +1,7 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int (input('Digite um número: '))
+dobro= n*2
+triplo= n*3
+rq= n**(1/2)
+print ('O número escolhido foi {}.\nO dobro desse número é {}.\nO triplo é {}.\nE a raiz quadrada é {:.0f}.'.format(n,dobro,triplo,rq))
